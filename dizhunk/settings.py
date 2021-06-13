ba004ed32c807d26febc4bcce9e6b008cea9e674
iamprakashkhatri <padkhu@gmail.com>
2021-06-13 15:00:31 +0545

    push new changes

--- dizhunk/settings.py
+++ dizhunk/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = ')j@+%nchzfons8ddxh)!ueps2l!!)5iu(e2(4@-44#f!s(pfb*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
